d0ef30e18a3a9e71097b0d4764b52c4efa71bf6e
Nathan Gonsalves <iAmKaprekar@gmail.com>
2024-02-15 08:13:14 -0500

    Initial map of top-layer components and logic

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import _style from '../assets/style.css';
 
+import Authentication from './components/Authentication';
+import ListManager from './components/ListManager';
+import Loading from './components/Loading';
+import Sorting from './components/Sorting';
+
 const App = () => {
-  return <h1>Test</h1>
+  const [page, setPage] = useState('loading');
+
+  const authenticate = async() => {
+    const response = await fetch('/api/auth');
+    const body = await response.json();
+    setPage(body.err ? 'authentication' : 'listManager')
+  }
+
+  useEffect(() => {
+    authenticate();
+  }, [])
+
+  let renderedPage;
+  switch (page) {
+    case 'loading':
+      renderedPage = <Loading/>;
+      break;
+    case 'authentication':
+      renderedPage = <Authentication/>;
+      break;
+    case 'listManager':
+      renderedPage = <ListManager/>;
+      break;
+    case 'sorting':
+      renderedPage = <Sorting/>;
+      break;
+  }
+
+  return (
+    <div id='app'>
+      <h1>App</h1>
+      {renderedPage}
+    </div>
+  );
 }
 
 const root = createRoot(document.querySelector('#root'));
---
+++ client/components/ListManager.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListManager = () => {
+  return (
+    <div id='listManager'>
+      <h1>ListManager</h1>
+    </div>
+  )
+}
+
+export default ListManager;
