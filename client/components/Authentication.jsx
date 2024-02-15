0afd50f3097c671e47363aba42ce74459903c2c6
Nathan Gonsalves <iAmKaprekar@gmail.com>
2024-02-15 09:38:54 -0500

    Began linking for auth frontend

--- client/components/Authentication.jsx
+++ client/components/Authentication.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
 
 const Authentication = () => {
+  const [newAccount, setNewAccount] = useState(true);
+
+  const switchPage = () => {
+    setNewAccount(!newAccount);
+  }
+
+  const renderedPage = newAccount ? <Signup/> : <Login/>;
+
   return (
     <div id='authentication'>
       <h1>Authentication</h1>
+      {renderedPage}
     </div>
   )
 }
