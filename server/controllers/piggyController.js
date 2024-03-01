6f6c4527f1811141c170333053289729ef99bb3f
Nathan Gonsalves <iamkaprekar@gmail.com>
2024-03-01 14:43:09 -0500

    Added piggybank controller middleware

--- server/controllers/piggyController.js
+++ server/controllers/piggyController.js
@@ -2,22 +2,16 @@ const controller = {};
 
 const db = require('../models/piggyModel');
 
-controller.isComplete = async() => {
+controller.addEntry = async(req, res, next) => {
   try {
+    const { coin } = req.body;
+    const addQuery = `INSERT INTO Entries (coin) VALUES ($1);`;
+    await db.query(addQuery, [coin]);
     return next();
   } catch (err) {
     return next({
-
-    })
-  }
-}
-
-controller.addEntry = async() => {
-  try {
-    return next();
-  } catch (err) {
-    return next({
-
+      log: `Error caught in piggyController.addEntry: ${err}`,
+      message: {err: 'Unknown error occurred in depositing coin.'}
     })
   }
 }
--- server/routers/piggyRouter.js
+++ server/routers/piggyRouter.js
@@ -3,10 +3,6 @@ const { Router } = require('express');
 const router = Router();
 const piggyController = require('../controllers/piggyController');
 
-router.get('/', piggyController.isComplete, (_, res) => {
-  res.status(200).json({complete: res.locals.complete});
-})
-
 router.post('/', piggyController.addEntry, (_, res) => {
   res.status(200).json({});
 })
